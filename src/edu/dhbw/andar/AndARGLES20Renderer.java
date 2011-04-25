f44262caf30f3f7bed3b389faa5e8fee41d51435
griffyboy0 <griffyboy0@gmail.com>
2011-04-25 05:07:12 +0000

    Completed front face.  I don't think its entirely there yet, but it's good enough for government work.

--- src/edu/dhbw/andar/AndARGLES20Renderer.java
+++ src/edu/dhbw/andar/AndARGLES20Renderer.java
@@ -73,6 +73,7 @@ public class AndARGLES20Renderer extends AndARRenderer {
 	private int muMVPMatrixHandle;
 	private int maPositionHandle;
 	private int maTextureHandle;
+	private int mFrameBuffer;
 	
 	/**
 	 * mode, being either GLES20.GL_RGB or GLES20.GL_LUMINANCE
@@ -140,7 +141,13 @@ public class AndARGLES20Renderer extends AndARRenderer {
 		if(customRenderer != null)
 			customRenderer.initGL(null);
 		
+		// Tell GL what to look at
 		Matrix.setLookAtM(mVMatrix, 0, 0, 0, 0, 0.0f, 0.0f, 5.0f, 0f, 1.0f, 0.0f);
+		
+		// Create a FrameBuffer for possible later use
+		int[] framebuffers = new int[1];
+		GLES20.glGenFramebuffers(1, framebuffers, 0);
+		mFrameBuffer = framebuffers[0];
 	}
 
 	/* (non-Javadoc)
@@ -151,7 +158,6 @@ public class AndARGLES20Renderer extends AndARRenderer {
 		GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 		GLES20.glUseProgram(mProgram);
 		GraphicsUtil.checkGlError("glUseProgram");
-	
 		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
 		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureName);
 		
@@ -243,16 +249,257 @@ public class AndARGLES20Renderer extends AndARRenderer {
 	
 	/**
 	 * Generates and loads a localized cubemap for object defined by vertices
-	 * @param n n*8 = number of triangles on front face
 	 * @param ssbb a screen space bounding box
 	 */
 	public void generateCubemap( float[] ssbb ) {
-		// TODO: Write this method
+		// Cubemap parameters
 		int n = edu.dhbw.andar.Config.CUBEMAP_QUALITY;
+		float percentEnlarge = 0.05f;
+		float percentBorder = 0.15f;
+		
+		// Render to Texture ModelView and Projection matrices
+	    float[] projmatrix = new float[16]; // Projection Matrix
+		
 		// Correct SSBB dimensions
-		// Generate front face geometry
-		// Generate texture coordinates for front face
+		float[] correctSSBB = { 0.0f, 0.0f, 0.0f, 0.0f };
+		float width = ssbb[2] - ssbb[0];
+		float height = ssbb[3] - ssbb[1];
+		float cx = ssbb[0] + ( width / 2.0f );
+		float cy = ssbb[1] + ( height / 2.0f );
+		width *= ( 1.0f + percentEnlarge );
+		height *= ( 1.0f + percentEnlarge );
+		correctSSBB[0] = ( cx - ( width / 2.0f ) < percentBorder ) ? percentBorder : cx - ( width / 2.0f );
+		correctSSBB[1] = ( cy - ( height / 2.0f ) < percentBorder ) ? percentBorder : cy - ( height / 2.0f );
+		correctSSBB[2] = ( cx + ( width / 2.0f ) > 1.0f - percentBorder ) ? 1.0f - percentBorder : cx + ( width / 2.0f );
+		correctSSBB[3] = ( cy + ( height / 2.0f ) > 1.0f - percentBorder ) ? 1.0f - percentBorder : cy + ( height / 2.0f );
+		
+		// Set up GL for RTT rendering
+		//GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+		GLES20.glUseProgram(mProgram);
+		GraphicsUtil.checkGlError("glUseProgram");
+		GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
+		GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureName);
+		Matrix.orthoM(projmatrix, 0, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, 1.0f);
+		Matrix.multiplyMM(mMVPMatrix, 0, projmatrix, 0, mVMatrix, 0);
+        GLES20.glUniformMatrix4fv(muMVPMatrixHandle, 1, false, mMVPMatrix, 0);
+        GLES20.glUniform1i(mSamplerLoc, 0);
+        // Commented for debug purposes
+        //GLES20.glBindFramebuffer(GLES20.GL_FRAMEBUFFER, mFrameBuffer); 
+        
+        // Generate front face geometry and texture coords...  Here we go...
+        float[] frontFaceVerts = new float[ n * 8 * 3 * 3 ]; int vidx = 0;
+        float[] frontFaceUVs = new float[ n * 8 * 3 * 2 ]; int tidx = 0;
+        float widthcorrection = (float) previewFrameWidth / (float) textureSize;
+        float heightcorrection = (float) previewFrameHeight / (float) textureSize;
+        
+        float vw = 1.0f / ( float ) n;
+        float ow = cx / ( float )( n + 1 );
+        float iw = ( ( correctSSBB[0] / 2.0f ) + ( cx - correctSSBB[0] ) ) / ( float ) n;
+        float ac = correctSSBB[3] + ( ( 1.0f - correctSSBB[3] ) / 2.0f );
+        float is = correctSSBB[0] / 2.0f;
+        
+        // Zone 1
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = ( vw * i ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * i ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( ow * ( i + 1 ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = 1.0f * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * ( i + 1) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        }
+        
+        // Zone 3
+        ac = correctSSBB[1] / 2.0f;
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = ( vw * i ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * i ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * ( i + 1) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( ow * ( i + 1 ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = 0.0f * heightcorrection;
+        }
+        
+        // Zone 2
+        ow = ( 1.0f - cx ) / ( float ) ( n + 1 ); 
+        iw = ( 1.0f - ( 1.0f - cx ) - ( ( 1.0f - correctSSBB[2] ) / 2.0f ) ) / ( float ) n;
+        ac = correctSSBB[3] + ( ( 1.0f - correctSSBB[3] ) / 2.0f );
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = ( vw * i );
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( iw * i ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( ow * ( i + 1 ) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = 1.0f * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) );
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( iw * ( i + 1 ) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        }
+        
+        // Zone 4
+        ac = correctSSBB[1] / 2.0f;
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = ( vw * i );
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( iw * i ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) );
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( iw * ( i + 1) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ac * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ( cx + ( ow * ( i + 1 ) ) ) * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = 0.0f * heightcorrection;
+        }
+        
+        // Zone 6
+        ow = cy / ( float )( n + 1 );
+        iw = ( ( correctSSBB[1] / 2.0f ) + ( cy - correctSSBB[1] ) ) / ( float ) n;
+        ac = correctSSBB[0] / 2.0f;
+        is = correctSSBB[1] / 2.0f;
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * i ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * i ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * ( i + 1) ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = 0.0f * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( ow * ( i + 1 ) ) * heightcorrection;
+        }
+        
+        // Zone 8
+        ac = correctSSBB[2] + ( ( 1.0f - correctSSBB[2] ) / 2.0f );
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * i ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( is + ( iw * i ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = 1.0f * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( ow * ( i + 1 ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) ) - 1.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] =  ( is + ( iw * ( i + 1) ) ) * heightcorrection;
+        }
+        
+        // Zone 5
+        ow = ( 1.0f - cy ) / ( float ) ( n + 1 ); 
+        iw = ( 1.0f - ( 1.0f - cy ) - ( ( 1.0f - correctSSBB[3] ) / 2.0f ) ) / ( float ) n;
+        ac = correctSSBB[0] / 2.0f;
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * i );
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( iw * i ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = -1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) );
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( iw * ( i + 1 ) ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = 0.0f * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( ow * ( i + 1 ) ) ) * heightcorrection;
+        }
+        
+        // Zone 7
+        ac = correctSSBB[2] + ( ( 1.0f - correctSSBB[2] ) / 2.0f );
+        for( int i = 0; i < n; i++ ) {
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * i );
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( iw * i ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 1.0f;
+        	frontFaceVerts[ vidx++ ] = ( vw * ( i + 1 ) );
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = ac * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( iw * ( i + 1 ) ) ) * heightcorrection;
+        	
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceVerts[ vidx++ ] = 0.0f;
+        	frontFaceUVs[ tidx++ ] = 1.0f * widthcorrection;
+        	frontFaceUVs[ tidx++ ] = ( cy + ( ow * ( i + 1 ) ) ) * heightcorrection;
+        }
+        
 		// Render front face into a cubemap texture via a framebuffer
+        FloatBuffer vfacebuffer = makeFloatBuffer( frontFaceVerts );
+        vfacebuffer.position(0);
+        GLES20.glVertexAttribPointer(maPositionHandle, 3, GLES20.GL_FLOAT, false,
+                TRIANGLE_VERTICES_DATA_STRIDE_BYTES, vfacebuffer);
+        GraphicsUtil.checkGlError("glVertexAttribPointer maPosition");
+        GLES20.glEnableVertexAttribArray(maPositionHandle);
+        GraphicsUtil.checkGlError("glEnableVertexAttribArray maPositionHandle");
+        
+        FloatBuffer tfacebuffer = makeFloatBuffer( frontFaceUVs );
+        tfacebuffer.position(0);
+        GLES20.glVertexAttribPointer(maTextureHandle, 2, GLES20.GL_FLOAT, false,
+        		TRIANGLE_VERTICES_UV_STRIDE_BYTES, tfacebuffer);
+        GraphicsUtil.checkGlError("glVertexAttribPointer maTextureHandle");
+        GLES20.glEnableVertexAttribArray(maTextureHandle);
+        GraphicsUtil.checkGlError("glEnableVertexAttribArray maTextureHandle");
+        
+		//GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 8 * n * 3 );
+		GraphicsUtil.checkGlError("glDrawArrays");
+        
 		// Create a new face for rendering remaining textures
 		// Generate remaining face texture coordinates
 		// Render remaining faces to cubemap textures via framebuffers
